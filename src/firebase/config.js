434d998420f3db7918eb00621bf8161bfa72e0b9
Azro1 <azro1.development@gmail.com>
2023-11-12 13:08:49 +0000

    Firebase Storage Setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import 'firebase/firestore'
 import 'firebase/auth'
+import 'firebase/storage'
 
 const firebaseConfig = {
     apiKey: "AIzaSyB7d1zG1uknySU0J2E50S7NWTVZEQ5pinE",
@@ -17,8 +18,9 @@ firebase.initializeApp(firebaseConfig);
 // init services
 const db = firebase.firestore()
 const auth = firebase.auth()
+const fileStore = firebase.storage()
 
 // timestamp
 const timestamp = firebase.firestore.Timestamp
 
-export { db, auth, timestamp }
+export { db, auth, fileStore, timestamp }
