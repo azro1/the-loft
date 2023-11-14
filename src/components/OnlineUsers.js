3b453715ad4e571d30c08cdb48513e847c214d3f
Azro1 <azro1.development@gmail.com>
2023-11-14 14:00:52 +0000

    Showing Users Online

--- src/components/OnlineUsers.js
+++ src/components/OnlineUsers.js
@@ -8,17 +8,22 @@ const Onlineusers = () => {
 const { error, documents } = useCollection('users')
 
   return (
-    <div className='user-list' >
-        <h2>All Users</h2>
-        {error && <div className='error'>{error}</div>}
-        {documents && documents.map((user) => (
-            <div className='user-list-item' key={user.id}>
-                <span>{user.displayName}</span>
-                <Avatar src={user.photoURL} />
-            </div>
+    <div className='user-list'>
+      <h2>All Users</h2>
+      {error && <div className='error'>{error}</div>}
+      {documents &&
+        documents.map((user) => (
+          <div className='user-list-item' key={user.id}>
+            <span
+              className='online-user'
+              style={{ background: user.online ? '#0ebb50' : '#d3d3d3' }}
+            ></span>
+            <span>{user.displayName}</span>
+            <Avatar src={user.photoURL} />
+          </div>
         ))}
     </div>
-  )
+  );
 }
 
 export default Onlineusers
