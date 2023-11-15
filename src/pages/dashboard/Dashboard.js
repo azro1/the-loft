656c474385b35675c139fc9d9ead77dbbbdd047e
Azro1 <azro1.development@gmail.com>
2023-11-15 18:19:17 +0000

    Fetching Projects

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,18 @@
+import ProjectList from '../../components/ProjectList'
+import { useCollection } from '../../hooks/useCollection'
+
 // styles
 import './Dashboard.css'
 
 const Dashboard = () => {
+  // destructure documents from hook
+  const { error, documents } = useCollection('projects')
+  
   return (
     <div>
-       Dashboard
+       <h2 className='page-title'>Dashboard</h2>
+       {error && <p className='error'>{error}</p>}
+       {documents && <ProjectList projects={documents} />}
     </div>
   )
 }
