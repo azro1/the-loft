2f923fd9f278ed827895142ee9d617d1cf30e006
Azro1 <azro1.development@gmail.com>
2023-11-19 15:42:35 +0000

    Project Details Page (fetching a project)

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -5,14 +5,20 @@ const useDocument = (collection, id) => {
   const [document, setDocument] = useState(null)
   const [error, setError] = useState(null)
 
-  // subscribe to realtime collection data
+  // subscribe to realtime docuemnt data
   useEffect(() => {
+    // reference
     const ref = db.collection(collection).doc(id)
 
+    // listener
     const unsubscribe = ref.onSnapshot((snapshot) => {
-      // update state
-      setDocument({ ...snapshot.data(), id: snapshot.id })
-      setError(null)
+      if (snapshot.data()) {
+        // update state
+        setDocument({ ...snapshot.data(), id: snapshot.id })
+        setError(null)
+      } else {
+        setError('that document does not exist.')
+      }
     }, (err) => {
       console.log(err.message)
       setError('Failed to get document.')
--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,10 +1,25 @@
+import { useParams } from 'react-router-dom'
+import { useDocument } from '../../hooks/useDocument'
+
 // styles
 import './Project.css'
 
 const Project = () => {
+  const { id } = useParams()
+  const { error, document } = useDocument('projects', id)
+  
+  if(error) {
+    return <div className="error">{error}</div>
+  }
+
+  if(!document) {
+    return <div className="loading">Loading...</div>
+  }
+
   return (
-    <div>
-       Project Details
+    <div className='project-details'>
+       <h1>{document.name}</h1>
+       
     </div>
   )
 }
