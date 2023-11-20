5979e864d276743a105fbd6112b80ca78e2f08f6
Azro1 <azro1.development@gmail.com>
2023-11-20 13:16:26 +0000

    Completing / Deleting Projects

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,10 +1,23 @@
 import Avatar from "../../components/Avatar"
+import { useFirestore } from "../../hooks/useFirestore"
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { useHistory } from 'react-router-dom'
 
 const ProjectSummary = ({ project }) => {
+  const { deleteDocument } = useFirestore('projects')
+  const { user } = useAuthContext()
+  const history = useHistory()
+
+  const handleClick = () => {
+    deleteDocument(project.id)
+    history.push('/')
+  }
+  
   return (
     <div>
       <div className='project-summary'>
         <h2 className='page-title'>{project.name}</h2>
+        <p>By: {project.createdBy.displayName}</p>
         <p className="due-date">Project due by: {project.dueDate.toDate().toDateString()}</p>
         <p className="details">{project.details}</p>
         <h4>Project is assigned to:</h4>
@@ -16,6 +29,9 @@ const ProjectSummary = ({ project }) => {
             ))}
         </div>
       </div>
+      {user.uid === project.createdBy.id && (
+          <button className="btn" onClick={handleClick}>Mark as Complete</button>
+        )}
     </div>
   )
 }
